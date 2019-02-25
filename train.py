4a794b957bb2f54493af24306bbd39cfbbf32068
HataFeng <fbjwying2@163.com>
2019-02-25 17:50:09 +0800

    tm

--- train.py
+++ train.py
@@ -27,11 +27,9 @@ def main():
 
 
 if __name__ == "__main__":
-
-    if os_type == "tinymind":
-        print('current working dir [{0}]'.format(os.getcwd()))
-        w_d = os.path.dirname(os.path.abspath(__file__))
-        print('change wording dir to [{0}]'.format(w_d))
-        os.chdir(w_d)
+    print('current working dir [{0}]'.format(os.getcwd()))
+    w_d = os.path.dirname(os.path.abspath(__file__))
+    print('change wording dir to [{0}]'.format(w_d))
+    os.chdir(w_d)
 
     main()
